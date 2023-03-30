2cbc7041f5da7f3fa786d70ddaafd96d6776aab6
Gabriele Simoni <46279871+HotCoffee04@users.noreply.github.com>
2023-03-30 23:08:44 +0200

    Added automatic SDL library setup script

--- compile.bat
+++ compile.bat
@@ -1,4 +1,2 @@
-gcc -static main.c game.c frac.c common.c -I"include" -L"lib" -lSDL2 -lm -lmingw32 -mwindows -Wl,--dynamicbase -Wl, -Wl, -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid -o frac.exe
+C:\MinGW\bin\gcc.exe -static main.c game.c frac.c common.c -I"C:\SDL2-2.26.4-mingw\i686-w64-mingw32\include" -L"C:\SDL2-2.26.4-mingw\i686-w64-mingw32\lib" -lSDL2 -lm -lmingw32 -mwindows -Wl,--dynamicbase -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid -o frac.exe
 pause
-::gcc main.c game.c frac.c common.c -g -I"include" -L"lib" -lSDL2_ttf -lSDL2 -o frac.exe
-::pause
